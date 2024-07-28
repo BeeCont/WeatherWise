a00142f0f48371ec27289640fdeaded0c8b69b46
Yegor Belik <yegor.belik.coder@gmail.com>
2024-07-28 03:51:39 +0300

    test: expand tests with success and error cases
    Add successful tests to ensure correct parsing of JSON
    responses. These tests include verifying the correct
    temperature, weather type, city name, sunrise, and sunset
    times. Extend the error tests to cover scenarios where the service
    fails. This will help in better understanding the service's structure
    and its robustness against various error conditions.
    Introduce fixtures for loading mock data, making it easier
    to reuse mock responses without manually writing them in
    each test.
    
    One test for sunparsing did not pass. This needs
    to be fixed.

--- tests/services/test_openweather_service.py
+++ tests/services/test_openweather_service.py
@@ -1,28 +1,35 @@
 import pytest
+import json
+import os
 import requests
 from unittest.mock import Mock, patch
 from requests.exceptions import RequestException
 from datetime import datetime
+
 from exceptions.exceptions import OpenWeatherServiceError
 from entities.weather import Weather, WeatherType
 from entities.coordinates import Coordinates
 from services.openweather_service import OpenWeatherService
+from config.settings import BASE_DIR
 
 @pytest.fixture
 def service():
     locator = Coordinates(latitude=40.7128, longitude=-74.0060)
     return OpenWeatherService(locator=locator)
 
-def test_get_weather_success(service):
+@pytest.fixture
+def load_fixture():
+    def _load_fixture(filename):
+        filepath = str(BASE_DIR) + f'/fixtures/{filename}'
+        with open(filepath, 'r') as file:
+            return json.load(file)
+    return _load_fixture
+    
+def test_get_weather_success(service, load_fixture):
 
     mock_response = Mock()
     mock_response.status_code = 200
-    mock_response.json.return_value = {
-        'main': {'temp': 22.8},
-        'weather': [{'id': 800}],
-        'sys': {'sunrise': 1596242400, 'sunset': 1596292800},
-        'name': 'New York'
-    }
+    mock_response.json.return_value = load_fixture('openweather_success_response.json')
 
     with patch.object(requests, 'get', return_value=mock_response):
 
@@ -36,16 +43,55 @@ def test_get_weather_success(service):
         assert weather.sunrise == datetime.fromtimestamp(1596242400)
         assert weather.sunset == datetime.fromtimestamp(1596292800)
 
+def test_get_weather_error(service, load_fixture):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = load_fixture('openweather_invalid_data_response.json')
+
+    with patch.object(requests, 'get', return_value=mock_response):
+        with pytest.raises(OpenWeatherServiceError, match=''):
+            service.get_weather()
+
 def test_get_weather_retries_on_failure(service):
     with patch.object(requests, 'get', side_effect=OpenWeatherServiceError("Error")):
         with pytest.raises(OpenWeatherServiceError):
             service.get_weather()
 
-def test_make_request_error(service):
-    with patch.object(OpenWeatherService, '_check_response', return_value=Mock(status_code=200, json=lambda: {})):
+def test_make_request_success(service, load_fixture):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = load_fixture('openweather_success_response.json')
+    
+    with patch.object(requests, 'get', return_value=mock_response):
+        with patch.object(OpenWeatherService, '_check_response', return_value=mock_response.json()):
+            result = service._make_request()
+            expected_result = load_fixture('openweather_success_response.json')
+            assert result == expected_result
+
+def test_make_request_error(service, load_fixture):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = load_fixture('openweather_success_response.json')
+
+    with patch.object(OpenWeatherService, '_check_response', return_value=mock_response.json()):
         with patch.object(requests, 'get', side_effect=RequestException("Request failed")):
             with pytest.raises(OpenWeatherServiceError, match="Error while executing request"):
                 service._make_request()
+
+def test_check_response_success(service):
+
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {}
+
+    with patch.object(OpenWeatherService, '_parse_json', return_value=mock_response.json()):
+        with patch.object(requests, 'get', return_value=mock_response):
+                result = service._check_response(mock_response)
+                assert result == {}
+
 
 def test_check_response_error(service):
 
@@ -56,6 +102,14 @@ def test_check_response_error(service):
         with pytest.raises(OpenWeatherServiceError, match="HTTP request error"):
             service._check_response(mock_response)
 
+def test_parse_json_success(service, load_fixture):
+
+    mock_response = Mock()
+    mock_response.json.return_value = load_fixture('openweather_success_response.json')
+    
+    result = service._parse_json(mock_response)
+    assert result == load_fixture('openweather_success_response.json')
+
 def test_parse_json_error(service):
 
     mock_response = Mock()
@@ -65,21 +119,30 @@ def test_parse_json_error(service):
         with pytest.raises(OpenWeatherServiceError, match="JSON parsing error"):
             service._parse_json(mock_response)
 
-def test_parse_weather_type(service):
+def test_parse_weather_type_success(service, load_fixture):
 
-    weather_dict = {'weather': [{'id': 800}]}
+    weather_dict = load_fixture('openweather_success_response.json')
     weather_type = service._parse_weather_type(weather_dict)
     assert weather_type == WeatherType.CLEAR
-    
-    weather_dict = {'weather': [{'id': 999}]}
+
+def test_parse_weather_type_error(service, load_fixture):
+
+    weather_dict = load_fixture('openweather_invalid_data_response.json')
     with pytest.raises(OpenWeatherServiceError):
         service._parse_weather_type(weather_dict)
 
-def test_parse_sun_time(service):
+def test_parse_sun_time_success(service, load_fixture):
 
-    weather_dict = {'sys': {'sunrise': 1596242400, 'sunset': 1596292800}}
+    weather_dict = load_fixture('openweather_success_response.json')
     sunrise = service._parse_sun_time(weather_dict, 'sunrise')
     sunset = service._parse_sun_time(weather_dict, 'sunset')
     
     assert sunrise == datetime.fromtimestamp(1596242400)
     assert sunset == datetime.fromtimestamp(1596292800)
+
+def test_parse_sun_time_error(service, load_fixture):
+
+    weather_dict = load_fixture('openweather_invalid_data_response.json')
+    with pytest.raises(OpenWeatherServiceError):
+        sunrise = service._parse_sun_time(weather_dict, 'sunrise')
+        sunset = service._parse_sun_time(weather_dict, 'sunset')
